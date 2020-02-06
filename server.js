54618664dfff5051520d3e00533209881fe25243
s1ep0y <danmol04@gmail.com>
2020-02-06 14:00:10 +0300

    base layer

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/'));
+
+const port =  process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
